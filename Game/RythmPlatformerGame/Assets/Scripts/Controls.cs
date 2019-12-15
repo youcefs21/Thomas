91c0847c69db2a5024c73b3b6090f98ff2868628
Youcef <34604972+youcefs21@users.noreply.github.com>
2019-12-14 19:03:48 -0500

    first proto

---
+++ Game/RythmPlatformerGame/Assets/Scripts/Controls.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public static float speed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float straffe = Input.GetAxis("Horizontal") * speed*10;
+        straffe *= Time.deltaTime;
+
+
+        transform.Translate(straffe, 0, 0);
+    }
+}
---
+++ Game/RythmPlatformerGame/Assets/Scripts/Moving.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(2f * Time.deltaTime, 0, 0);
+    }
+
+
+
+
+
+}
