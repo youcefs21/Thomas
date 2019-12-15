7007e2095dc00cff30d547dc3ed125cef9d63dca
Youcef <34604972+youcefs21@users.noreply.github.com>
2019-12-15 00:49:34 -0500

    TimeCount

---
+++ Game/RythmPlatformerGame/Assets/Scripts/TimeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+public class TimeManager : MonoBehaviour
+{
+    TimeSpan ts;
+    Stopwatch stopWatch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
+        ts = stopWatch.Elapsed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ts = stopWatch.Elapsed;
+        UnityEngine.Debug.Log(ts.TotalSeconds);
+
+    }
+}
